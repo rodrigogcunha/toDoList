37fc2f228c38025cf59c6f0ad947a3ad9ce684f9
Rodrigo Cunha <rodrigocunha@live.com>
2023-09-09 14:51:00 -0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-btn");
+
+// Funções
+const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
+
+  const cancelEditBtn = document.createElement("button");
+  cancelEditBtn.classList.add("edit-todo");
+  cancelEditBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(cancelEditBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deleteBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
+  todoInput.focus()
+};
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = todoInput.value;
+
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
+});
